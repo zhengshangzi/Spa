508f998fbf87f24295015d2d79bf1f2daa9e8aaf
郑尚姿 <33643927+zhengshangzi@users.noreply.github.com>
2020-04-04 17:40:39 +0800

    Add files via upload

---
+++ 63-click-history/index.js
@@ -0,0 +1,26 @@
+$(function() {
+    var $btn = $('.main input');
+  
+    var n = 0;
+  
+    setBtnTitle(n);
+  
+    $btn.click(function() {
+      var url = location.origin + location.pathname + '#/' + (++n);
+  
+      history.pushState(null, null, url);
+      setBtnTitle(n);
+    });
+  
+    $(window).on('popstate', function() {
+      n = (location.href).match(/(\d*)$/)[0];
+  
+      n = (n === '' ? 0 : n);
+      setBtnTitle(n);
+    });
+  
+    function setBtnTitle(n) {
+      $btn.val('被按了 ' + n + ' 次');
+    }
+  });
+  
